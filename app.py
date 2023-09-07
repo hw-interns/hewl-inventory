4eec886451dcec49f340ca3ee05dc0e7b89ae302
Winbert Zhang <winbertzhang@gmail.com>
2023-09-06 21:39:21 -0700

    Update requirements

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from flask import jsonify
 from datetime import datetime
 
